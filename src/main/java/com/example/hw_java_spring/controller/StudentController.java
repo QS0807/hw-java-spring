043116b259238a5ba290a774eb743c5f4cfb8dee
Xizhe Li <77379215+QS0807@users.noreply.github.com>
2024-06-19 20:17:20 -0400

    Update CrossOrigin

--- src/main/java/com/example/hw_java_spring/controller/StudentController.java
+++ src/main/java/com/example/hw_java_spring/controller/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://3.82.46.135")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/students")
